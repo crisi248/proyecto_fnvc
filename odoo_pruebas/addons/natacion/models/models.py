651dbe1255ae60a3d9f271e7c34a2d8ad17df9a6
Cristian <cristianbratanov@gmail.com>
2025-09-26 19:06:16 +0200

    Commit 3

--- odoo_pruebas/addons/natacion/models/models.py
+++ odoo_pruebas/addons/natacion/models/models.py
@@ -9,6 +9,7 @@ class club(models.Model):
     name = fields.Char(required=True)
     town = fields.Char()
     partner = fields.Char()
+    championships = fields.Many2many("natacion.championship")
 
 class category(models.Model):
     _name = "natacion.category"
@@ -18,6 +19,7 @@ class category(models.Model):
     minAge = fields.Integer()
     maxAge = fields.Integer()
     swimmers_list = fields.One2many("natacion.swimmer", "category_id")
+    tests = fields.One2many("natacion.test", "category_id")
 
 class swimmer(models.Model):
     _name = "natacion.swimmer"
@@ -29,38 +31,57 @@ class swimmer(models.Model):
     category_id = fields.Many2one("natacion.category", ondelete="set null")
     bestTime = fields.Float()
     bestStyle = fields.One2many("natacion.style", "bestSwimmers")
+    sessions = fields.Many2many("natacion.session")
+    tests = fields.Many2many("natacion.test")
 
 class style(models.Model):
     _name = "natacion.style"
-    _description = "Nadador"
+    _description = "Estilo de natación"
 
     name = fields.Char(required=True)
     bestSwimmers = fields.Many2one("natacion.swimmer", ondelete="set null")
+    tests = fields.One2many("natacion.test", "style_id")
     
 class championship(models.Model):
     _name = "natacion.championship"
-    _description = "Nadador"
+    _description = "Campeonato de natación"
 
     name = fields.Char(required=True)
-    date = fields.Datetime( string='Start Date')
+    clubs = fields.Many2many("natacion.club")
+    # Nadadores inscritos de un club que esta inscrito
+    start_date = fields.Datetime()
+    end_date = fields.Datetime()
+    sessions = fields.One2many("natacion.session", "championship_id")
 
 class session(models.Model):
     _name = "natacion.session"
     _description = "Sesión de natación"
 
     name = fields.Char(required=True)
+    date = fields.Datetime()
+    championship_id = fields.Many2one("natacion.championship", ondelete="set null")
+    tests = fields.One2many("natacion.test", "session_id")
+    swimmers = fields.Many2many("natacion.swimmer")
 
 class test(models.Model):
     _name = "natacion.test"
     _description = "Test de natación"
 
     name = fields.Char(required=True)
+    description = fields.Char(required=True)
+    style_id = fields.Many2one("natacion.style", ondelete="set null")
+    category_id = fields.Many2one("natacion.category", ondelete="set null")
+    swimmers = fields.Many2many("natacion.swimmer")
+    sets = fields.One2many("natacion.set", "test_id")
+    session_id = fields.Many2one("natacion.sesion", ondelete="set null")
 
 class set(models.Model):
     _name = "natacion.set"
-    _description = "Serie de natación"
+    _description = "Set de natación"
 
     name = fields.Char(required=True)
+    test_id = fields.Many2one("natacion.test", ondelete="set null")
+
 
 
 
